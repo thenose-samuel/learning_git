53fbf8a9377619d7963cc90b2c810138c6b734db
Samuel Khongthaw <skthaw1@gmail.com>
2021-05-04 21:53:21 +0530

    Update test.c
    Added a function for modulus operation

--- test.c
+++ test.c
@@ -14,6 +14,12 @@ int product(int a, int b){
 	}
 	return 0;
 }
+int modulo(int a, int b){
+	if(a>0 && b>0){
+		return a%b;
+	}
+	return 0;
+}
 int main(){
 	int result = sum(5,2);
 	int result2 = product(5,2)
